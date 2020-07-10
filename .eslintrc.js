2ca60793d0809f918b732cba73abc5c90bcd8ac9
QW5pbWE <narutow21@interia.pl>
2020-07-10 08:04:21 +0200

    Project structure

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ['airbnb', 'airbnb/hooks'],
+
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-console': 'error',
+  },
+};
